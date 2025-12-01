0d3beb6cb488ed70be6c483b19830cfd558e9f60
Bulciu Adrian <bulciu.adrian@gmail.com>
2025-12-01 17:20:15 +0200

    2025 day 1 php

---
+++ 2025/day_1/day_1.php
@@ -0,0 +1,33 @@
+<?php
+
+$turns = file("./input.txt", FILE_IGNORE_NEW_LINES);
+$start = 50;
+$result = 0;
+
+$start = 50;
+$result = 0;
+
+foreach ($turns as $turn) {
+    $n = (int) substr($turn, 1);
+
+    if ($turn[0] == "L") {
+        $start -= $n;
+        if ($start < 0) {
+            $start = (100 + ($start % 100)) % 100;
+        }
+    }
+    else {
+        $start += $n;
+
+        if ($start > 99) {
+            $start %= 100;
+        }
+    }
+
+    if ($start == 0) {
+        $result++;
+    }
+}
+    
+
+print_r($result);
